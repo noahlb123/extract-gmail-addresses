6c0125e093959dbf336f8b66e77f53872286656e
Noah Liguori-Bills <noah.liguori-bills@uconn.edu>
2023-10-31 13:52:08 -0400

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ def extract(s):
 	l = s.split(" ")
 	o = []
 	for e in l:
-		email = e.replace("<","").replace(">","")
+		email = e.replace("<","").replace(">","").replace(";",",")
 		if "@" in email and email not in o:
 			o.append(email)
 	for i in o:
